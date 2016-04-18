6fcee85a2882f962fa972fda377fb6775e43c7a0
snupa <bunta.adrian@gmail.com>
2016-04-18 13:28:29 +0300

    updated configuration style

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 const unloq = require('unloq'),
   initModels = require('./lib/initModels');
-const PLUGIN_URL = 'https://plugin.unloq.io/login.js';
 /**
  * Created by Adrian on 08-Apr-16.
  * UNLOQ.io authhorization middleware for thorin.
@@ -21,25 +20,28 @@ module.exports = function(thorin, opt, pluginName) {
     },
     registration: true,         // IF we do not have the user locally, we create it.
     sessionPlugin: 'session',   // setting it to null will not handle automatica remote logout
-    apiKey: null,         // the UNLOQ API Key
-    apiSecret: null       // the UNLOQ API Secret
+    key: null,         // the UNLOQ API Key
+    secret: null       // the UNLOQ API Secret
   }, opt);
   const logger = thorin.logger(opt.logger);
   const modelLoader = initModels(thorin, opt);
-  if(!opt.apiKey) {
+  if(!opt.key) {
     logger.fatal('Missing API Key');
   }
-  if(!opt.apiSecret) {
+  if(!opt.secret) {
     logger.fatal('Missing API Secret');
   }
 
   var apiObj = new unloq.Api({
-    key: opt.apiKey,
-    secret: opt.apiSecret
+    key: opt.key,
+    secret: opt.secret,
+    gateway: opt.gateway,
+    endpoint: "",
+    version: opt.version || "1"
   });
   /* Require our authorization actions */
   thorin.loadPath(__dirname + '/lib/authorization', thorin, opt, apiObj);
-
+  const PLUGIN_URL = 'https://api.unloq.io/login.js';
   /* Expose the setup() function */
   apiObj.setup = function(done) {
     if(!modelLoader.setup) return done();
@@ -53,7 +55,7 @@ module.exports = function(thorin, opt, pluginName) {
   /* Returns the full login <script> tag location*/
   apiObj.getScript = function(theme) {
     if(!theme) theme = 'light';
-    return '<script type="text/javascript" src="'+PLUGIN_URL+'" data-unloq-theme="'+theme+'" data-unloq-key="'+opt.apiKey+'"></script>';
+    return '<script type="text/javascript" src="'+PLUGIN_URL+'" data-unloq-theme="'+theme+'" data-unloq-key="'+opt.key+'"></script>';
   }
   return apiObj;
 };
